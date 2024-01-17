ba10fba2504d06b388d8ffdca4c77d804193bd93
John Penney <john.penney@gmail.com>
2024-01-17 18:41:08 +0000

    Update clamd_setup.sh
    adding usefull comments and installing wget as was missing on my PoC

--- clamd_setup.sh
+++ clamd_setup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+#used for testing downloading virus from internet
+yum install -y wget
+#calmav is located in the epel 
 yum install -y epel-release
 yum install -y clamav-server clamav-data clamav-update clamav-filesystem clamav clamav-scanner-systemd clamav-devel clamav-lib clamav-server-systemd
+
+#testing upload is the simulating the customer sftp mount infected is the mount where the viruses are moved 
 mkdir /upload
 mkdir /infected
+
+#configuring the start up for clamav 
 mv /usr/lib/systemd/system/clamd@.service /usr/lib/systemd/system/clamd.service
 
 cat > /usr/lib/systemd/system/clamd.service <<EOF
@@ -33,6 +40,7 @@ ExecStart=/usr/sbin/clamonacc -F --config-file=/etc/clamd.d/scan.conf --move=/in
 WantedBy=multi-user.target
 EOF
 
+#importing and enbabling the services 
 systemctl daemon-reload
 systemctl enable clamd.service
 systemctl enable clamav-clamonacc.service
@@ -48,6 +56,7 @@ sed -i '/OnAccessIncludePath \/home/a OnAccessIncludePath \/upload/'  /etc/clamd
 sed -i 's/#OnAccessPrevention /OnAccessPrevention /' /etc/clamd.d/scan.conf
 sed -i 's/#OnAccessExtraScanning /OnAccessExtraScanning /' /etc/clamd.d/scan.conf
 
+#starting the services 
 systemctl start clamd.service && systemctl start clamav-freshclam.service && systemctl start clamav-clamonacc.service
 #systemctl stop clamd.service && systemctl stop clamav-freshclam.service && systemctl stop clamav-clamonacc.service
 
