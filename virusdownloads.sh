6df983cf69beff823c2ca29a028777f26f22a3b8
John Penney <john.penney@gmail.com>
2024-01-17 18:37:09 +0000

    Create virusdownloads.sh
    list of common virus test

---
+++ virusdownloads.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget -q https://www.cybersoft.com/static/downloads/eicar/eicar2.com
+wget -q https://www.cybersoft.com/static/downloads/eicar/eicar2.vdl
+wget -q https://www.cybersoft.com/static/downloads/eicar/eicar2_nonstd.com
+wget -q https://www.cybersoft.com/static/downloads/eicar/eicar2_nonstd.vdl
+wget -q https://www.cybersoft.com/static/downloads/eicar/eicar1.com
+wget -q https://www.cybersoft.com/static/downloads/eicar/eicar1.vdl
