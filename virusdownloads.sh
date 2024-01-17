c352085fc06536d5e5395d02676e0a8990dbc26b
John Penney <john.penney@gmail.com>
2024-01-17 20:07:33 +0000

    Update virusdownloads.sh
    locations added

--- virusdownloads.sh
+++ virusdownloads.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget -q https://www.cybersoft.com/static/downloads/eicar/eicar2.com
-wget -q https://www.cybersoft.com/static/downloads/eicar/eicar2.vdl
-wget -q https://www.cybersoft.com/static/downloads/eicar/eicar2_nonstd.com
-wget -q https://www.cybersoft.com/static/downloads/eicar/eicar2_nonstd.vdl
-wget -q https://www.cybersoft.com/static/downloads/eicar/eicar1.com
-wget -q https://www.cybersoft.com/static/downloads/eicar/eicar1.vdl
+wget -q https://www.cybersoft.com/static/downloads/eicar/eicar2.com -O /infected/eicar2.com
+wget -q https://www.cybersoft.com/static/downloads/eicar/eicar2.vdl -O /infected/eicar2.vdl
+wget -q https://www.cybersoft.com/static/downloads/eicar/eicar2_nonstd.com -O /infected/eicar2_nonstd.com
+wget -q https://www.cybersoft.com/static/downloads/eicar/eicar2_nonstd.vdl -O /infected/eicar2_nonstd.vdl
+wget -q https://www.cybersoft.com/static/downloads/eicar/eicar1.com -O /infected/eicar1.com
+wget -q https://www.cybersoft.com/static/downloads/eicar/eicar1.vdl -O /infected/eicar1.vdl 
